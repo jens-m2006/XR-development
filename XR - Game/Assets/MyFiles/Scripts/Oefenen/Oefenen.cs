8c7391e3c861a1a3faa7fb02b6595ac4e284c4f2
Jens <jens.maerevoet@student.kdg.be>
2026-05-08 13:45:23 +0200

    Test to see if branches work

--- XR - Game/Assets/MyFiles/Scripts/Oefenen/Oefenen.cs
+++ XR - Game/Assets/MyFiles/Scripts/Oefenen/Oefenen.cs
@@ -7,6 +7,11 @@ public class Oefenen : MonoBehaviour
     
     [SerializeField] private GameObject object1;
     public float points = 3;
+
+    public GameObject[] enemies;
+    public Color playerColor;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
